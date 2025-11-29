bc0997b06a0f085ce2ae297008c32f3a9246ae19
K-Akira928 <k.akirarei928@gmail.com>
2025-11-29 16:16:57 +0900

    linter

--- src/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { hello } from "./index.ts";
-
-describe("hello", () => {
-  it("returns hello world!!", () => {
-    expect(hello()).toBe("hello world!!");
-  });
-});
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-export function hello(): string {
-	return "hello world!!";
-}
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (_, res) => {
+	res.json({ message: "Hello Express!" });
+});
+
+app.listen(port, () => {
+	console.log(`Server is running at http://localhost:${port}`);
+});
